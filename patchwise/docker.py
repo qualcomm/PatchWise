2a9e972549608f2fcd4f329399d780fd86c350b6
David Gantman <david.gantman@oss.qualcomm.com>
2025-09-15 13:16:24 -0700

    docker.py: Remove duplicate license
    Signed-off-by: David Gantman <david.gantman@oss.qualcomm.com>

--- patchwise/docker.py
+++ patchwise/docker.py
@@ -1,9 +1,6 @@
 # Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 # SPDX-License-Identifier: BSD-3-Clause
 
-# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
-# SPDX-License-Identifier: BSD-3-Clause
-
 import logging
 import os
 import subprocess
