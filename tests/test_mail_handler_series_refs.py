9b027f69c25a4b0998c69f46a3c32e7657f571f0
Aditya Chillara <achillar@qti.qualcomm.com>
2026-08-29 02:05:40 +0530

    tests: mail_handler: add copyright
    Signed-off-by: Aditya Chillara <achillar@qti.qualcomm.com>

--- tests/test_mail_handler_series_refs.py
+++ tests/test_mail_handler_series_refs.py
@@ -1,3 +1,6 @@
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause
+
 from email.message import EmailMessage
 from pathlib import Path
 from typing import Optional
